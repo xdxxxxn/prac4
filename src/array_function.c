10416c60657d0c7d487cba1814e15691c7af040a
Барышева Виктория Вадимовна <admin@localhost.localdomain>
2025-10-27 16:57:59 +0700

    изменен

--- src/array_function.c
+++ src/array_function.c
@@ -1,8 +1,53 @@
 #include "../include/array_function.h"
 
-void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {}
+void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {
 
-void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {}
+  for (size_t i = 0; i < size_src1; i++) {
+      dest[i] = src1[i];
+  }
 
-void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {}
+  for (size_t i = 0; i < size_src2; i++) {
+      dest[size_src1 + i] = src2[i];
+  }
+
+}
+
+void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
+
+  for (size_t i = 0; i < size_src1; i++) {
+      dest[i] = src1[i];
+  }
+
+  for (size_t i = 0; i < size_src2; i++) {
+      dest[size_src1 + i] = src2[i];
+  }
+
+  size_t total_size = size_src1 + size_src2;
+  for (size_t i = 0; i < total_size - 1; i++) {
+      for (size_t j = 0; j < total_size - i - 1; j++) {
+          if (dest[j] > dest[j + 1]) {
+              int temp = dest[j];
+              dest[j] = dest[j + 1];
+              dest[j + 1] = temp;
+          }
+      }
+  }
+
+}
+
+void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
+
+  *min = src[0];
+  *max = src[0];
+
+  for (size_t i = 1; i < size; i++) {
+      if (src[i] < *min) {
+          *min = src[i];
+      }
+      if (src[i] > *max) {
+          *max = src[i];
+      }
+  }
+
+}
 
